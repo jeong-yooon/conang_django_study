1caaa7cda85e09c724c0e5e70ebf1c9677e480bc
jeong-yooon <chjy1819@gmail.com>
2023-08-06 22:14:56 +0900

    장고강의 6강

--- Django/pjt/sProject/students/urls.py
+++ Django/pjt/sProject/students/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
 
+app_name= 'students'
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("students/", include('students.urls')),
+    path('reg/', views.regStudents, name='reg'),
+    path('regCon/', views.regConStudents, name='regCon'),
+    path('all/', views.readStudentAll, name='stuAll'),
 ]
--- Django/pjt/sProject/students/views.py
+++ Django/pjt/sProject/students/views.py
@@ -1,3 +1,26 @@
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.shortcuts import render
+from students.models import Student
+
 
 # Create your views here.
+def regStudents(request):
+    return render(request, 'students/registerStudent.html')
+
+def regConStudents(request):
+    name = request.POST['name']
+    major = request.POST['major']
+    age = request.POST['age']
+    grade = request.POST['grade']
+    gender = request.POST['gender']
+
+    qs = Student(s_name=name, s_major=major, s_age=age, s_grade=grade, s_gender=gender)
+    qs.save() # db 에 저장해준다.
+
+    return HttpResponseRedirect(reverse('students:stuAll'))
+
+def readStudentAll(request):
+    qs = Student.objects.all()
+    context = {'student_list': qs}
+    return render(request, 'students/readStudents.html', context)
